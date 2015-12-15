ee21520e683ce541590bacc0f9f4c0c780a6c215
= <tsmit291@icloud.com>
2015-12-15 12:47:58 -0700

    map printed on page

---
+++ js/visualization.js
@@ -0,0 +1,9 @@
+// function initialize(){
+//   map = new google.maps.Map(document.getElementById('map'),{
+//     zoom: 2,
+//     center: new google.maps.LatLng(2.8,-187.3),
+//     mapTypeId: google.maps.mapTypeId.SATELLITE
+//   });
+//   var script = document.createElement('script');
+//   script.src = ''
+// }
