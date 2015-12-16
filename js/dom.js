2eac848232e05b69eac416a4ebc5add0b82f224c
= <tsmit291@icloud.com>
2015-12-15 20:38:06 -0700

    census api loaded and map printing

--- js/dom.js
+++ js/dom.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){})
+$(document).ready(function(){
+
   function init(){
-  var mapOptions = {
-    center: new google.maps.LatLng(40.7827, -73.965),
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    zoom: 2
-  };
-  var venueMap;
-  venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
+    var mapOptions = {
+      center: new google.maps.LatLng(39.000, -73.965),
+      mapTypeId: google.maps.MapTypeId.SATELLITE,
+      zoom: 2
+    };
+    var venueMap;
+    venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
   function loadScript(){
     var script = document.createElement('script');
     script.src='http://maps.googleapis.com/maps/api/js?callback=init';
     document.body.appendChild(script);
-};
-  window.onload = loadScript;
-  console.log("this shit is working");
+  };
+  window.onload = loadScript();
+})
--- js/visualization.js
+++ js/visualization.js
@@ -1,9 +1,26 @@
-// function initialize(){
-//   map = new google.maps.Map(document.getElementById('map'),{
-//     zoom: 2,
-//     center: new google.maps.LatLng(2.8,-187.3),
-//     mapTypeId: google.maps.mapTypeId.SATELLITE
-//   });
-//   var script = document.createElement('script');
-//   script.src = ''
-// }
+google.load('visualization', '1', {packages: ['geomap']});
+
+   $(function() {
+       // when document loads, grab the json
+     $.getJSON('http://api.census.gov/data/2010/sf1?key=8f256976ce53a25c5b283fa02daeae5fa80fd034&get=P0010001,NAME&for=state:*', function(data) {
+       // once grabbed, we run this callback
+
+       // setup the new map and its variables
+       var map = new google.visualization.DataTable();
+         map.addRows(data.length);  // length gives us the number of results in our returned data
+         map.addColumn('string', 'State');
+           map.addColumn('number', 'Population');
+
+       // now we need to build the map data, loop over each result
+       $.each(data, function(i,v) {
+         // set the values for both the name and the population
+         map.setValue(i, 0, v.Population);
+         map.setValue(i, 1, v.State);
+       });
+       // finally, create the map!
+       var geomap = new google.visualization.GeoMap(
+             document.getElementById('visualization'));
+            geomap.draw(map, null);
+
+     });
+  });
